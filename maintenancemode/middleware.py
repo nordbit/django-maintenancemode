afa34512b81c81aea6efbece06474147b3417172
Rafał Mirończyk <voyageur.pl@gmail.com>
2013-09-25 11:45:15 +0200

    Added maintenance mode trigger in django admin

--- maintenancemode/middleware.py
+++ maintenancemode/middleware.py
@@ -7,6 +7,8 @@ import django
 from django.conf import settings
 from django.core import urlresolvers
 
+from .models import MaintenanceMode
+
 
 if django.VERSION[:2] <= (1, 3):
     from django.conf.urls import defaults as urls
@@ -19,13 +21,17 @@ from maintenancemode.conf.settings.defaults import (
 urls.handler503 = 'maintenancemode.views.defaults.temporary_unavailable'
 urls.__all__.append('handler503')
 
-IGNORE_URLS = tuple([re.compile(url) for url in MAINTENANCE_IGNORE_URLS])
+IGNORE_URLS = [re.compile(url) for url in MAINTENANCE_IGNORE_URLS]
 
 
 class MaintenanceModeMiddleware(object):
     def process_request(self, request):
         # Allow access if middleware is not activated
-        if not MAINTENANCE_MODE:
+        enabled_maintenance_modes = \
+            MaintenanceMode.objects.filter(enable_maintenance_mode=True)
+        enabled_maintenance_mode = \
+            enabled_maintenance_modes[0] if enabled_maintenance_modes else None
+        if not MAINTENANCE_MODE and not enabled_maintenance_mode:
             return None
 
         # Allow access if remote ip is in INTERNAL_IPS
@@ -34,9 +40,18 @@ class MaintenanceModeMiddleware(object):
 
         # Allow access if the user doing the request is logged in and a
         # superuser.
+        superuser_from_model_enabled = enabled_maintenance_mode.allow_superuser
         ALLOW_SU = getattr(settings, 'MAINTENANCE_ALLOW_SU', False)
-        if ALLOW_SU and hasattr(request, 'user') and request.user.is_superuser:
+        if (
+                (ALLOW_SU or superuser_from_model_enabled) and
+                hasattr(request, 'user') and
+                request.user.is_superuser
+        ):
             return None
+
+        # update list of ignored urls with ignored url from model
+        for ignore_url in enabled_maintenance_mode.ignored_urls:
+            IGNORE_URLS.append(re.compile(ignore_url.url))
 
         # Check if a path is explicitly excluded from maintenance mode
         for url in IGNORE_URLS:
