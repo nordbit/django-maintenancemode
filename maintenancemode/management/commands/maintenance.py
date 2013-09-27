30b21594209f3c714e0dc69f19a762cb8f98e5cf
Paweł Stiasny <pawel.stiasny@21s.pl>
2013-09-27 15:36:20 +0200

    Added a list option.

--- maintenancemode/management/commands/maintenance.py
+++ maintenancemode/management/commands/maintenance.py
@@ -9,17 +9,29 @@ class Command(BaseCommand):
     args = '<maintenance_id> (on|off)'
     help = 'Toggle maintenance mode on or off'
 
-    def handle(self, pk=None, state=None, **kwargs):
-        if not pk or not state in ['on', 'off']:
-            raise CommandError('maintenance %s' % self.args)
+    def _list_modes(self):
+        self.stderr.write('Available maintenance modes:\n')
+        for available in MaintenanceMode.objects.all():
+            self.stderr.write(str(available.pk) + '. ' + str(available) + '\n')
+
+    def handle(self, *args, **options):
+        if len(args) == 1 and args[0] == 'list':
+            self._list_modes()
+            return
+
+        if len(args) < 2:
+            raise CommandError(
+                'invalid arguments\n'
+                'maintenance list\n'
+                'maintenance %s' % self.args)
+
+        pk = args[0]
+        state = args[1]
 
         try:
             mm = MaintenanceMode.objects.get(pk=pk)
         except MaintenanceMode.DoesNotExist:
-            self.stderr.write('Available maintenance modes:\n')
-            for available in MaintenanceMode.objects.all():
-                self.stderr.write(
-                    str(available.pk) + '. ' + str(available) + '\n')
+            self._list_modes()
             raise CommandError('invalid maintenance id')
         mm.enable_maintenance_mode = (state == 'on')
         mm.save()
